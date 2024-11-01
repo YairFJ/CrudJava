a53fb3f6657ad9676deaf2dae73f497e30dcd671
Yali <farinayair1@gmail.com>
2024-10-31 22:25:18 -0300

    Metodo DELETE

--- src/main/java/com/api/Messages/Services/MessageService.java
+++ src/main/java/com/api/Messages/Services/MessageService.java
@@ -47,5 +47,9 @@ public class MessageService {
         return null;
     }
 
+    public boolean deleteMessage (Long id){
+        return messages.removeIf(msg -> msg.getId().equals(id));
+    }
+
 
 }
